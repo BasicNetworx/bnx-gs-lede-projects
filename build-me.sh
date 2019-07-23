33e15d4b274f4e774046c11187261162db96bc65
Brant Rosenberger <brant@room3327.com>
2019-07-23 13:16:45 -0400

    Update build script to line break after each config line

--- build-me.sh
+++ build-me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'CONFIG_TARGET_ar71xx=y CONFIG_TARGET_ar71xx_generic=y CONFIG_TARGET_ar71xx_generic_DEVICE_oolitebox=y' > .config
+echo -e 'CONFIG_TARGET_ar71xx=y\nCONFIG_TARGET_ar71xx_generic=y\nCONFIG_TARGET_ar71xx_generic_DEVICE_oolitebox=y\n' > .config
 make defconfig
 make -n -j2 | tee buildlog.log
