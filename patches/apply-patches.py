5b1d18fa63f9b79687ff1019640da5240019af0a
Chris Coble <chris@ascentops.com>
2020-04-16 14:11:01 -0400

    update patches script for prettier output

--- patches/apply-patches.py
+++ patches/apply-patches.py
@@ -9,9 +9,14 @@ CONFIG_PATH = os.path.join(PATCH_PATH, "config.json")
 def main():
     with open(CONFIG_PATH) as cfg_fs:
         cfg = json.load(cfg_fs)
+
+    print("Installing patches:")
     for k,v in cfg.items():
+        print("{} -> {}".format(k,v))
         src = os.path.join(PATCH_PATH, k)
-        dst = os.path.join(OPENWRT_ROOT_PATH, v)
+        dst = os.path.join(OPENWRT_ROOT_PATH, v, "patches")
+        if not os.path.exists(dst):
+            os.mkdir(dst)
         shutil.copy(src, dst)
 
 if __name__ == "__main__":
