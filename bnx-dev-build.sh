9928cdfcfe42c838feca4a34a03bf0e2499da283
Chris Coble <chris@ascentops.com>
2020-04-17 13:38:53 -0400

    update build config
    Select BNX packages only.  The BNX packages will select
    packages that are dependencies.

--- bnx-dev-build.sh
+++ bnx-dev-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-./scripts/feeds update bnx packages luci
+./scripts/feeds update -a
 ./scripts/feeds install -p bnx -a
-./scripts/feeds install -p luci luci
 python patches/apply-patches.py
 cp bnx-dev-build.config .config
 make defconfig
