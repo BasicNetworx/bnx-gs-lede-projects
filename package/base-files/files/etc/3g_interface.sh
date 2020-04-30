d22151c2772c0d3e0d35e429b007738664b7f035
Chris Coble <chris@ascentops.com>
2020-04-30 13:32:23 -0400

    do not try to create sim interface if it already exists

--- package/base-files/files/etc/3g_interface.sh
+++ package/base-files/files/etc/3g_interface.sh
@@ -1,9 +1,26 @@
-uci set network.ppp0='interface'
-uci set network.ppp0.proto='3g'
-uci set network.ppp0.service='umts'
-#uci set network.3g.username='root'
-#uci set network.3g.password='123456'
-uci set network.ppp0.device='/dev/ttyUSB3'
-uci set network.ppp0.apn='internet'
-uci commit
+. /lib/functions.sh
 
+add_iface() {
+    iface=$1
+    dev=$2
+
+    uci set network.$iface='interface'
+    uci set network.$iface.proto='3g'
+    uci set network.$iface.service='umts'
+    uci set network.$iface.device=$dev
+    uci set network.$iface.apn='internet'
+}
+
+check_add_iface() {
+    iface=$1
+    dev=$2
+    config_get result $iface proto
+    if [ -z $result ]; then
+        add_iface $iface $dev
+    fi
+}
+
+config_load network
+check_add_iface "Sim1" "/dev/ttyUSB3"
+check_add_iface "Sim2" "/dev/ttyUSB7"
+uci commit
