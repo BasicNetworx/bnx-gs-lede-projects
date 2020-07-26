9f2833c6f1ff24f7a23e737f09049aef91761265
Chris Coble <chris@ascentops.com>
2020-07-26 19:20:42 -0400

    circleci: use prebuilt toolchain

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,19 +3,23 @@ set -e -x
 
 ENV=$1
 COMMITISH=$2
+PREBUILT_URL="https://s3.amazonaws.com/download.bnxcloud.com/tools/prebuilt.tar.bz2"
 
 version=$(bash .circleci/get_version.sh $ENV $COMMITISH)
 
 ## FETCH
 ./scripts/feeds update -a
 ./scripts/feeds install -p bnx -a
+if [ ! -x prebuilt.tbz ]; then
+    wget $PREBUILT_URL -O prebuilt.tbz
+    tar jxf prebuilt.tbz
+fi
 
 ## CONFIGURE
 cp bnx.config .config
-cat .circleci/toolchain.config >> .config
 cat .circleci/$ENV.config >> .config
 echo 'CONFIG_VERSION_NUMBER="'$version'"' >> .config
 make defconfig
 
 ## BUILD
-make -j12
+make -j12 package/compile package/install target/compile target/install
