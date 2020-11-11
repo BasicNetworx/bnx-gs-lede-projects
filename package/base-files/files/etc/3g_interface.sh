3ac606d6d82d9de9cb20c85cfa3e034af7f02dbf
Chris Coble <chris@ascentops.com>
2020-11-11 17:41:04 -0500

    Use qmi for sim network interfaces

--- package/base-files/files/etc/3g_interface.sh
+++ package/base-files/files/etc/3g_interface.sh
@@ -6,8 +6,8 @@ add_iface() {
     dev=$2
 
     uci set network.$iface='interface'
-    uci set network.$iface.proto='3g'
-    uci set network.$iface.service='umts'
+    uci set network.$iface.ipv6='0'
+    uci set network.$iface.proto='qmi'
     uci set network.$iface.device=$dev
     uci set network.$iface.apn='internet'
     uci set network.$iface.metric=$((metric_offset))
@@ -24,6 +24,6 @@ check_add_iface() {
 }
 
 config_load network
-check_add_iface "Sim1" "/dev/ttyUSB3"
-check_add_iface "Sim2" "/dev/ttyUSB7"
+check_add_iface "Sim1" "/dev/cdc-wdm0"
+check_add_iface "Sim2" "/dev/cdc-wdm1"
 uci commit
